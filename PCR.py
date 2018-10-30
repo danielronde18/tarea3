a0578eb798a30199595fe962a14ef8a0a570d255
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2018-10-30 15:37:02 -0500

    Update PCR.py

--- PCR.py
+++ PCR.py
@@ -2,3 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy.linalg 
 
+archivo= np.genfromtxt('WDBC.txt', delimiter=',',dtype='U16')
+
+
+arc=archivo.shape
+
+for i in range(arc[0]):
+	for j in range(arc[1]):
+		if(j>=2):
+			archivo[i][j].astype(float)
+
+print(archivo)
+
