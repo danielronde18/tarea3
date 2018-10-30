69f67fad355f56e72b61279e1e8eaac9a2410eb2
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2018-10-30 15:38:26 -0500

    Update PCR.py

--- PCR.py
+++ PCR.py
@@ -14,3 +14,20 @@ for i in range(arc[0]):
 
 print(archivo)
 
+datos=archivo[:,2:].astype(float)
+indicador=archivo[:,:2]
+
+
+def cov_matrix(mat):
+    dim = np.shape(mat)[1]
+    numpunt = np.shape(mat)[0]
+    cov = np.ones([dim,dim])
+    for i in range(dim):
+        for j in range(dim):
+            mediafila = np.mean(mat[:,i])
+            mediacolum = np.mean(mat[:,j])
+            cov[i,j] = np.sum((mat[:,i]-mediafila) * (mat[:,j]-mediacolum)) / (numpunt -1)
+    return cov
+
+print(cov_matrix(datos))
+
