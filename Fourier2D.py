3f657fbf17f45dcd2b86dda6a348cddef4e1e7ce
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2018-10-31 16:03:07 -0500

    Update Fourier2D.py

--- Fourier2D.py
+++ Fourier2D.py
@@ -8,5 +8,16 @@ plt.figure()
 plt.imshow(imagen,plt.cm.gray)
 plt.title('imagen original')
 
+from scipy import fftpack
+imagen_fft=fftpack.fft2(imagen)
 
+def plot_transformada(imagen_fft):
+	from matplotlib.colors import LogNorm
+	plt.imshow(np.abs(imagen_fft), norm=LogNorm(vmin=5))
+	plt.colorbar()
+
+plt.figure()
+plot_transformada(imagen_fft)
+plt.title('transformada de fourier')
+plt.savefig('ronderoscarlos_FT2D.pdf.')
 
