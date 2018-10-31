1772588c6140cef0f1d1dcfd6bfab1b6aa35e46b
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2018-10-31 15:19:27 -0500

    Add files via upload

---
+++ Fourier2D.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+imagen=plt.imread('/home/daniel/tarea3ronderoscarlos/Arboles.png').astype(float)
+
+plt.figure()
+plt.imshow(imagen,plt.cm.gray)
+plt.title('imagen original')
+
+from scipy import fftpack
+imagen_fft=fftpack.fft2(imagen)
+
+def plot_transformada(imagen_fft):
+	from matplotlib.colors import LogNorm
+	plt.imshow(np.abs(imagen_fft), norm=LogNorm(vmin=5))
+	plt.colorbar()
+
+plt.figure()
+plot_transformada(imagen_fft)
+plt.title('transformada de fourier')
+plt.savefig('ronderoscarlos_FT2D.pdf.')
+	
+fraccion=0.1
+
+imagen_fft2=imagen_fft.copy()
+r,c= imagen_fft2.shape
+imagen_fft2[int(r*fraccion):int(r*(1-fraccion))]=0
+imagen_fft2[:, int(c*fraccion):int(c*(1-fraccion))]=0
+
+plt.figure()
+plot_transformada(imagen_fft2)
+plt.title('filtrado')
+plt.savefig('ronderoscarlos_FT2D_filtrada.pdf.')
+
+imagen_nueva=fftpack.ifft2(imagen_fft2).real
+
+plt.figure()
+plt.imshow(imagen_nueva,plt.cm.gray)
+plt.title('imagen reconstruida')
+plt.savefig('ronderoscarlos_Imagen_filtrada.pdf')
+
