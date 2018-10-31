60524994689d177b2c7ec8f2542130743f4dd775
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2018-10-31 17:24:21 -0500

    Update Fourier.py

--- Fourier.py
+++ Fourier.py
@@ -13,26 +13,6 @@ plt.savefig('ronderoscarlos_signal.pdf')
 
 
 
-def frequency(n, d = 1.0):
-	f = np.arange(0, n//2 + 1)/(d*n)
-	return f
-
-def transformadafourier(archivo):
-	N = len(archivo[:,0])
-	num = np.linspace(0,0, N)
-	for l in range(len(archivo[:,0])):
-		for j in range(len(archivo[:,1])):
-			num[l] = (archivo[j,1]*np.exp(-1j*2*np.pi*l*j/N)).sum()
-		return num
-
-paso=archivo[1,1]-archivo[0,1]
-frecuencia=fftfreq(len(archivo[0]),paso)
-
-plt.figure()
-plt.plot(frecuencia,abs(transformadafourier(archivo)))
-plt.title('transformada')
-plt.savefig('')
-
 
 
 
