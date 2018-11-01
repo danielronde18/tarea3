56cc4fbc74afc58b78f43e81ad413e15ecfb1085
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2018-10-31 23:22:12 -0500

    Update Fourier.py

--- Fourier.py
+++ Fourier.py
@@ -11,7 +11,17 @@ plt.plot(archivo[:,0],archivo[:,1])
 plt.title('señal')
 plt.savefig('ronderoscarlos_signal.pdf')
 
+x=archivo[:,0]
+y=archivo[:,1]
 
+def transformadafourier(x,y):
+	N = len(x)
+	num = np.linspace(0,0, N)
+	for l in range(len(x)):
+		for j in range(len(y)):
+			num[l] = y[j]*np.exp((-1j)*2*np.pi*l*j/N)
+	num=num/N
+	return num
 
 
 
